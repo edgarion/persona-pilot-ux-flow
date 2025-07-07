8a52b31570ca0f25725e96b22d0f8344ea91e62e
ed <edgarcostilla@gmail.com>
2025-07-07 21:18:09 +0200

    Fix UserTests component default export

---
+++ src/components/UserTests.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserTests: React.FC = () => {
+  return (
+    <div>
+      <h2>User Tests</h2>
+      <p>User tests component placeholder</p>
+    </div>
+  );
+};
+
+export default UserTests;
